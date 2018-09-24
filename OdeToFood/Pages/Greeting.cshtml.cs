1043cf5ac712841c49a02b5c847f67bdbb6a2035
dsergiu77 <sergiu.dauceanu@googlemail.com>
2018-09-24 10:35:31 +0200

    Add Razor Pages

---
+++ OdeToFood/Pages/Greeting.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Services;
+
+namespace OdeToFood.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private readonly IGreeter _greeter;
+        public string CurrentMessage { get; set; }
+
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public void OnGet(string name)
+        {
+            CurrentMessage = $"{name}: {_greeter.GetMessageOfTheDay()}";
+        }
+    }
+}
